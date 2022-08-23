0c75d1083527ef607d3cb500947ef80880591abb
Matteo Susanna <matteo2784@gmail.com>
2022-08-23 14:17:16 +0200

    Fine

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyTitle />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyTitle from './components/MyTitle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyTitle
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/MyTitle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="box">
+    <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTitle',
+}
+</script>
+
+<style scoped>
+
+.box{
+    background-color: black;
+    width: 600px;
+    margin: auto;
+    border-radius: 13px
+}
+
+.box:hover{
+    background-color: red;
+}
+
+h1{
+    color:red;
+}
+
+h1:hover{
+    color:black
+}
+
+</style>
+
+
+
